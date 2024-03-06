78db2d3adc8f27a7fa5e33c967c4e7b1fbae6afd
Om-Sureja <omsureja@gmail.com>
2024-03-06 22:29:56 +0530

    vercel file

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/User");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const database = require("./config/database");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+dotenv.config();
+database.connect();
+
+app.use("/user", userRouter);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
